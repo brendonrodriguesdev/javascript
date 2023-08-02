0a30d4faf7cceab3615ebf7519c62972fc634449
Brendon Rodrigues <brendonalex.r@gmail.com>
2023-08-02 16:09:33 -0300

    Continuação da aula 12

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,9 +1,12 @@
-let hora = 24
+let agora = new Date()
+let hora = agora.getHours()
 console.log(`Agora são exatamente ${hora} horas.`)
 if (hora < 6) {
+    console.log('Boa madrugada!')
+} else if (hora < 12) {
     console.log('Bom dia!')
-} else if (hora <= 18) {
-    console.log('Boa tarde!')
+} else if (hora < 18) {
+    console.log("Boa tarde")
 } else {
     console.log('Boa noite!')
 }
