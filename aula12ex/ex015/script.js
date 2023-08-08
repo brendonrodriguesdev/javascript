1bcf157168288de45516d0efc3c5578e43b78c0c
Brendon Rodrigues <brendonalex.r@gmail.com>
2023-08-08 15:43:35 -0300

    Exercício concluído

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -6,6 +6,44 @@ function verificarIdade() {
     if (fano.value.length == 0 || fano.value > ano) {
         alert('[ERRO] Verifique os dados e tente novamente!')
     }else {
-        alert('tudo ok')
+        let fsex = document.getElementsByName('sexo');
+        let idade = ano - Number(fano.value);
+        let genero = ''
+        let img = document.createElement('img');
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'imagens/foto-bebe-homem.jpg')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'imagens/foto-jovem-homem.jpg')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'imagens/foto-adulto-homem.jpg')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'imagens/foto-idoso-homem.jpg')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'imagens/foto-bebe-mulher.jpg')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'imagens/foto-jovem-mulher.jpg')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'imagens/foto-adulta-mulher.jpg')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'imagens/foto-idosa-mulher.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
