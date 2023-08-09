2b1b29a7f4fc3aae6156522e6dcff101c7933483
Brendon Rodrigues <brendonalex.r@gmail.com>
2023-08-09 11:34:54 -0300

    aula de estrutura de repetições, while e do while

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+/*
+
+Estrututa de repetição com teste lógico no início
+
+let contador = 1
+while(contador <= 6) {
+    console.log(`Passo ${contador}`);
+    contador++
+}
+*/
+
+let contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 6)
