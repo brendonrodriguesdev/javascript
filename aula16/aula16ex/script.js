af62f7fb8e96c802f06e92f4990db5cd7ccac951
Brendon Rodrigues <brendonalex.r@gmail.com>
2023-08-19 00:35:34 -0300

    Início da função do segundo botão!

--- aula16/aula16ex/script.js
+++ aula16/aula16ex/script.js
@@ -1,21 +1,34 @@
-let button = document.getElementById("button");
+const button = document.getElementById("button");
 button.addEventListener("click", add);
 
-let div = document.getElementById("res");
-let select = document.getElementById("select");
+const div = document.getElementById("res");
+const select = document.getElementById("select");
+
+const paragraph = document.createElement('p');
+paragraph.setAttribute('id', 'parag');
+const list = [];
 
 function add() {
   let inputNumber = document.getElementById("number");
-  let inpNumber = Number(inputNumber.value);
 
-  let list = [];
-
-  if (inpNumber === 0) {
+  if (inputNumber.value == 0) {
     return alert("Valor inválido ou já encontrado na lista!");
   } else {
     let item = document.createElement("option");
+    let inpNumber = Number(inputNumber.value);
     item.text = `Valor ${inpNumber} adicionado.`;
     select.appendChild(item);
-    return list = inpNumber;
+    return list.push(inpNumber);
+  }
+}
+
+function finalize() {
+  if (list.length === 0) {
+    alert("Adicione valores antes de finalizar!");
+  } else {
+    paragraph.innerHTML = `Ao todo temos ${list.length} valores cadastrados!`
+    res.appendChild(paragraph);
+
+    paragraph.innerHTML += `O maior valor informado foi ${list} `
   }
 };
