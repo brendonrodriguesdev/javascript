0e5672d1b5e49d80a0bfd61eb8e175295f8e9f19
Brendon Rodrigues <brendonalex.r@gmail.com>
2023-08-18 23:05:34 -0300

    Função adicionar números no select realizada!

--- aula16/aula16ex/script.js
+++ aula16/aula16ex/script.js
@@ -1,10 +1,21 @@
 let button = document.getElementById("button");
 button.addEventListener("click", add);
+
+let div = document.getElementById("res");
+let select = document.getElementById("select");
+
 function add() {
+  let inputNumber = document.getElementById("number");
+  let inpNumber = Number(inputNumber.value);
+
+  let list = [];
+
   if (inpNumber === 0) {
     return alert("Valor inválido ou já encontrado na lista!");
+  } else {
+    let item = document.createElement("option");
+    item.text = `Valor ${inpNumber} adicionado.`;
+    select.appendChild(item);
+    return list = inpNumber;
   }
-}
-
-let inputNumber = document.getElementById("number");
-let inpNumber = Number(inputNumber.value);
+};
