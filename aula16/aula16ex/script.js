6c93f200156e444857a1d8571640c20013e89d0d
Brendon Rodrigues <brendonalex.r@gmail.com>
2023-08-21 22:17:52 -0300

    Atualização no js

--- aula16/aula16ex/script.js
+++ aula16/aula16ex/script.js
@@ -4,14 +4,14 @@ button.addEventListener("click", add);
 const div = document.getElementById("res");
 const select = document.getElementById("select");
 
-const paragraph = document.createElement('p');
-paragraph.setAttribute('id', 'parag');
+const paragraph = document.createElement("p");
+paragraph.setAttribute("id", "parag");
 const list = [];
 
 function add() {
   let inputNumber = document.getElementById("number");
 
-  if (inputNumber.value == 0) {
+  if (inputNumber.value == 0 || inputNumber.value < 1 || inputNumber.value > 100) {
     return alert("Valor inválido ou já encontrado na lista!");
   } else {
     let item = document.createElement("option");
@@ -26,9 +26,39 @@ function finalize() {
   if (list.length === 0) {
     alert("Adicione valores antes de finalizar!");
   } else {
-    paragraph.innerHTML = `Ao todo temos ${list.length} valores cadastrados!`
-    res.appendChild(paragraph);
 
-    paragraph.innerHTML += `O maior valor informado foi ${list} `
-  }
+    paragraph.innerHTML = `Ao todo temos ${list.length} valores cadastrados! </br>`;
+    res.appendChild(paragraph);
+    
+    //maior valor
+    let bigValue = list[0];
+    for (let i = 1; i < list.length; i++) {
+      if (list[i] > bigValue) {
+        bigValue = list[i];
+      };
+    };
+
+    //menor valor
+    let lowerValue = list[0];
+    for ( let i = 1; i < list.length; i--) {
+      if (list[i] < lowerValue) {
+        lowerValue = list[i];
+      };
+    };
+
+    let sum = 0
+    for (let i = 0; i < list.length; i++) {
+      sum += valores[i];
+    };
+
+    let media = soma / list.length;
+
+    paragraph.innerHTML += `O maior valor informado foi ${bigValue} </br>`;
+
+    paragraph.innerHTML += `O menor valor informado foi ${lowerValue} </br>`;
+
+    paragraph.innerHTML += `A soma de todos os valores é ${sum} </br>`;
+
+    paragraph.innerHTML += `A média de todos os valores é ${media} </br>`
+  };
 };
