2305da730436efa6fb0c9db6cb60ae925464cc37
Brendon Rodrigues <brendonalex.r@gmail.com>
2023-08-28 23:58:54 -0300

    Exercício 010

---
+++ Desafios/desafio010/script.js
@@ -0,0 +1,12 @@
+function calcular() {
+    let valorA = prompt('Qual é o valor de A?');
+    let valorB = prompt("Qual é o valor de B?");
+    let valorC = prompt('Qual é o valor de C?');
+    let resultado = document.getElementById('res');
+
+    let bhaskara = (Number(valorB) ** 2) - 4 * Number(valorA) * Number(valorC);
+
+    resultado.innerHTML = `<p>A equação atual é ${valorA}x² + ${valorB}x + ${valorC} = 0</p>`;
+
+    resultado.innerHTML += `O valor calculado foi: ${bhaskara}`;
+}
