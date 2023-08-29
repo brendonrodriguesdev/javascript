bb2d87222cb093b77a59bc67dd636865a43fdf83
Brendon Rodrigues <brendonalex.r@gmail.com>
2023-08-29 00:26:39 -0300

    atualização no html do desafio 10

--- Desafios/desafio010/script.js
+++ Desafios/desafio010/script.js
@@ -6,7 +6,9 @@ function calcular() {
 
     let bhaskara = (Number(valorB) ** 2) - 4 * Number(valorA) * Number(valorC);
 
-    resultado.innerHTML = `<p>A equação atual é ${valorA}x² + ${valorB}x + ${valorC} = 0</p>`;
+    resultado.innerHTML = `<p>A equação atual é ${valorA}x² ${valorB}x ${valorC} = 0</p>`;
+
+    resultado.innerHTML += `<p>O cálculo realizado será ${valorB}² - 4 . ${valorA} . ${valorB}</p>`;
 
     resultado.innerHTML += `O valor calculado foi: ${bhaskara}`;
 }
