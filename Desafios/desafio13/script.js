650f83cf11d12ba90981142b10b0a7b4dc2c84c7
Brendon Rodrigues <brendonalex.r@gmail.com>
2023-09-06 09:51:53 -0300

    Conclusão do desafio 13

--- Desafios/desafio13/script.js
+++ Desafios/desafio13/script.js
@@ -1,7 +1,7 @@
 const button = document.getElementById("inputButton");
 button.addEventListener("click", calcMedia);
 
-let res = document.getElementById('res');
+let res = document.getElementById("res");
 
 function calcMedia() {
   let studentName = prompt("Qual é o nome do aluno?");
@@ -16,17 +16,14 @@ function calcMedia() {
 
   let media = (score1Formatted + score2Formatted) / 2;
 
-  res.innerHTML = `<p>Com as notas ${score1Formatted} e ${score2Formatted}, a média é ${media}.</p>`;
+  res.innerHTML += `<p>Com as notas ${score1Formatted} e ${score2Formatted}, a média é ${media}.</p>`;
 
-//   if (media > 6) {
-//     res.innerHTML += `aprovado!`;
-//   };
-
-//   if (media > 3 || media < 6) {
-//     res.innerHTML += `recuperação!`;
-//   };
-
-//   if (media < 3) {
-//     res.innerHTML += `Reprovado!`;
-//   };
+  if (media >= 6) {
+    res.innerHTML += `Com média acima de 6,0, o aluno está <span class="approved">aprovado!</span>`;
+    
+  } else if (media < 3) {
+    res.innerHTML += `Com média abaixo de 3,0, o aluno está <span class="disapproved">reprovado!</span>`;
+  } else {
+    res.innerHTML += `Com média entre 3,0 e 6,0, o aluno está em <span class="recuperation">recuperação!</span>`;
+  };
 };
--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Criado para estudos de javascript pelo curso em vídeo
 |[Aula 11b](./aula11/ex009.js)|[Desafio 10](./Desafios/desafio10/desafio10.html)|
 |[Aula 11c](./aula11/ex011.html)|[Desafio 11](./Desafios/desafio11/desafio11.html)|
 |[Aula 11d](./aula11/exTESTE.html)|[Desafio 12](./Desafios/desafio12/desafio12.html)|
-|[Aula 12a](./aula12/ex011.js)||
+|[Aula 12a](./aula12/ex011.js)|[Desafio 13](./Desafios/desafio13/desafio13.html)|
 |[Aula 12b](./aula12/ex012.js)||
 |[Aula 12c](./aula12/ex013.js)||
 |[Aula 12Exa](./aula12ex/ex014/modelo.html)||
